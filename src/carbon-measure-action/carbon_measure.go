a6a08af9e67e79418c843cedae37860520ff9537
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2022-02-28 16:49:24 +0000

    feat(gh-action): demonstrates how total emissions could be forecast in a pipeline

--- src/carbon-measure-action/carbon_measure.go
+++ src/carbon-measure-action/carbon_measure.go
@@ -6,13 +6,30 @@ import (
 )
 
 func main() {
-	infrastructureAsCodeFileType := os.Getenv("file_type")
+	infraFileType := os.Getenv("file_type")
 	// TODO: For terraform, we might need to accept a list of multiple files
-	infrastructureAsCodeFileName := os.Getenv("file_location")
-	infrastructureAsCodeZoneKey := os.Getenv("zone_key")
-	fmt.Printf("Unused: %v %v %v\n", infrastructureAsCodeFileName, infrastructureAsCodeFileType, infrastructureAsCodeZoneKey)
+	infraFileName := os.Getenv("file_location")
+	electricityMapZoneKey := os.Getenv("zone_key")
+	totalKwh := iterateOverFile(infraFileName, infraFileType)
+	averageKwh := getCarbonIntensity(electricityMapZoneKey)
 	// this comes from electricity map
-	gitHubOutputVariable("grams_carbon_equivalent_per_kwh", "200")
-	gitHubOutputVariable("grams_over_24h", "4800")
+	carbonIntensity := float64(averageKwh) * totalKwh
+	gitHubOutputVariable("grams_carbon_equivalent_per_kwh", fmt.Sprint(averageKwh))
+	gitHubOutputVariable("grams_emitted_over_24h", fmt.Sprint(carbonIntensity))
 	githubNoticeMessage("Successfully ran carbon measure action.")
+}
+
+func getCarbonIntensity(zoneKey string) int {
+	return 200
+	// TODO: Get the carbon intensity over 24 hours rather than just the current intensity
+}
+
+func getKwhForComponent(componentName string) float64 {
+	return 2.6
+}
+
+func iterateOverFile(fileName string, infraFileType string) float64 {
+	// TODO: Get kwh for each component and return a summed float
+	// TODO: Call a different iterator depending on if it is arm, bicep, terraform, pulumi, etc
+	return getKwhForComponent("component1") + getKwhForComponent("component2")
 }
