47c75d2e8b3d2ac8282fd69e3e2cc326475fdb9f
dennisdelamida <dennis.delamida@accenture.com>
2022-04-04 16:09:21 +0800

    Function for getting hourly CI rates for the past 24 hours on ElectricityMap Fixes #33

--- src/carbon-measure-action/carbon_measure.go
+++ src/carbon-measure-action/carbon_measure.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	electmap "main/pkg/electricitymap"
 	iac "main/pkg/iacARM"
 	"os"
 )
 
 func main() {
-	githubNoticeMessage("Starting carbon measure action.")
+	//SgithubNoticeMessage("Starting carbon measure action.")
 	infraFileType := os.Getenv("IACType")
 	infraFileName := os.Getenv("IACFile")
 	// TODO: For terraform, we might need to accept a list of multiple files
@@ -21,9 +22,13 @@ func main() {
 	averageKwh := getCarbonIntensity(electricityMapZoneKey)
 	// this comes from electricity map
 	carbonIntensity := float64(averageKwh) * totalKwh
-	gitHubOutputVariable("grams_carbon_equivalent_per_kwh", fmt.Sprint(averageKwh))
-	gitHubOutputVariable("grams_emitted_over_24h", fmt.Sprint(carbonIntensity))
-	githubNoticeMessage("Successfully ran carbon measure action.")
+	fmt.Println("grams_carbon_equivalent_per_kwh", averageKwh)
+	fmt.Println("grams_emitted_over_24h", carbonIntensity)
+	fmt.Println("Successfully ran carbon measure action.")
+
+	//gitHubOutputVariable("grams_carbon_equivalent_per_kwh", fmt.Sprint(averageKwh))
+	//gitHubOutputVariable("grams_emitted_over_24h", fmt.Sprint(carbonIntensity))
+	//githubNoticeMessage("Successfully ran carbon measure action.")
 }
 
 func processARMData(file string) {
@@ -38,7 +43,22 @@ func processARMData(file string) {
 }
 
 func getCarbonIntensity(zoneKey string) int {
-	return 200
+
+	cc := new(electmap.TypAPIParams)
+	cc.Zone = "US-CAL-CISO"
+	ccmap, _ := electmap.New(zoneKey).RecentCarbonIntensity(*cc)
+	return ccmap.History[len(ccmap.History)-1].CarbonIntensity //200
+	var x []TypResource
+	for s := range x {
+
+		for resourcedetail := range x[s].Location {
+
+			//	carbontotal = carbontotal + 1
+			//	totalwatt = totalwatt + 1
+			//	procesorcount = procesorcount + 1
+		}
+
+	}
 	// TODO: Get the carbon intensity over 24 hours rather than just the current intensity
 }
 
@@ -50,4 +70,58 @@ func iterateOverFile(fileName string, infraFileType string) float64 {
 	// TODO: Get kwh for each component and return a summed float
 	// TODO: Call a different iterator depending on if it is arm, bicep, terraform, pulumi, etc
 	return getKwhForComponent("component1") + getKwhForComponent("component2")
+}
+
+type TypARM struct {
+	Resources  []TypResource           `json:"resources"`
+	Parameters map[string]TypParameter `json:"parameters"`
+	Variables  map[string]string       `json:"variables"`
+}
+
+type TypResource struct {
+	Type       string         `json:"type"`
+	Location   string         `json:"location"`
+	SKU        typResourceSKU `json:"sku"`
+	Properties struct {
+		Template struct {
+			Resources []TypResource `json:"resources"`
+		} `json:"template"`
+	} `json:"properties"`
+}
+
+type typResourceSKU struct {
+	Name      string   `json:"name"`
+	Tier      string   `json:"tier"`
+	Size      string   `json:"size"`
+	Family    string   `json:"family"`
+	Capacity  int      `json:"capacity"`
+	Locations []string `json:"locations"`
+}
+
+type TypParameter struct {
+	Type          string      `json:"type"`
+	DefaultValue  string      `json:"defaultValue"`
+	AllowedValues []string    `json:"allowedValues"`
+	MinValue      int         `json:"minValue"`
+	MaxValue      int         `json:"maxValue"`
+	MinLength     int         `json:"minLength"`
+	MaxLength     int         `json:"maxLength"`
+	Metadata      typMetadata `json:"metadata"`
+}
+
+type typVariable struct {
+}
+
+type typMetadata struct {
+	Description string `json:"description"`
+}
+
+type typSummary struct {
+	resource string
+	details  []typSummaryDetails
+}
+
+type typSummaryDetails struct {
+	location string
+	count    int
 }
