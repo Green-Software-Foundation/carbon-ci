d54705a8134ddcc7a12e09cc87ac578b6688c9af
dennisdelamida <dennis.delamida@accenture.com>
2022-04-07 16:28:32 +0800

    Function for getting hourly CI rates for the past 24 hours on ElectricityMap Fixes #33

--- src/carbon-measure-action/carbon_measure.go
+++ src/carbon-measure-action/carbon_measure.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
-	//SgithubNoticeMessage("Starting carbon measure action.")
-	// wattTimeUser := os.Getenv("WATT_TIME_USER")
+	EM "main/pkg/electricitymap"
+	iac "main/pkg/infraascode"
+
+	//elec "main/pkg/electricitymap"
+	"os"
+)
+
+//SgithubNoticeMessage("Starting carbon measure action.")
+// wattTimeUser := os.Getenv("WATT_TIME_USER")
+// wattTimePass := os.Getenv("WATT_TIME_PASS")
+func main() {
+	//var grandaverageKwh int
+	//("Starting carbon measure action.")
+	//os.Setenv("IACType", "arm")
+	//os.Setenv("IACTemplateFile", "vm")
+	//os.Setenv("ELECTRICITY_MAP_AUTH_TOKEN", "3bhtgXSayVvgmuwEHry6zYYr")
 	infraFileType := os.Getenv("IACType")
 	infraFileName := os.Getenv("IACTemplateFile")
 	electricityMapZoneKey := os.Getenv("ELECTRICITY_MAP_AUTH_TOKEN")
 
 	// TODO: For terraform, we might need to accept a list of multiple files
 
-	iac.GetIACSummary(iac.TypIACQuery{Filetype: infraFileType, Filename: infraFileName})
+	//sumary := iac.GetIACSummary(iac.TypIACQuery{Filetype: infraFileType, Filename: infraFileName})
 
 	em := EM.New(electricityMapZoneKey)
 	em.LiveCarbonIntensity(EM.TypAPIParams{Zone: "US-CAL-CISCO"})
@@ -30,22 +44,23 @@ import (
 
 func getCarbonIntensity(zoneKey string) int {
 
-	cc := new(electmap.TypAPIParams)
-	cc.Zone = "US-CAL-CISO"
-	ccmap, _ := electmap.New(zoneKey).RecentCarbonIntensity(*cc)
-	return ccmap.History[len(ccmap.History)-1].CarbonIntensity //200
-	var x []TypResource
-	for s := range x {
+	//cc := new(electmap.TypAPIParams)
+	//cc.Zone = "US-CAL-CISO"
+	//ccmap, _ := electmap.New(zoneKey).RecentCarbonIntensity(*cc)
+	//return ccmap.History[len(ccmap.History)-1].CarbonIntensity //200
+	// var x []TypResource
+	// for s := range x {
 
-		for resourcedetail := range x[s].Location {
+	// 	for resourcedetail := range x[s].Location {
 
-			//	carbontotal = carbontotal + 1
-			//	totalwatt = totalwatt + 1
-			//	procesorcount = procesorcount + 1
-		}
+	// 		//	carbontotal = carbontotal + 1
+	// 		//	totalwatt = totalwatt + 1
+	// 		//	procesorcount = procesorcount + 1
+	// 	}
 
-	}
+	// }
 	// TODO: Get the carbon intensity over 24 hours rather than just the current intensity
+	return 200
 }
 
 func getKwhForComponent(componentName string) float64 {
@@ -55,7 +70,16 @@ func getKwhForComponent(componentName string) float64 {
 func iterateOverFile(fileName string, infraFileType string) float64 {
 	// TODO: Get kwh for each component and return a summed float
 	// TODO: Call a different iterator depending on if it is arm, bicep, terraform, pulumi, etc
-	return getKwhForComponent("component1") + getKwhForComponent("component2")
+	//var summary []TypSummary
+	println(fileName)
+	println(infraFileType)
+	var c int
+	summary := iac.GetIACSummary(iac.TypIACQuery{Filetype: infraFileType, Filename: fileName})
+	for _, ts := range summary {
+		c = ts.Count
+	}
+
+	return getKwhForComponent("component1") * float64(c)
 }
 
 type TypARM struct {
@@ -108,6 +132,21 @@ type typSummary struct {
 }
 
 type typSummaryDetails struct {
+	location string
+	count    int
+}
+
+type TypSummary struct {
+	resource string
+	sizes    []TypSizes
+}
+
+type TypSizes struct {
+	size    string
+	details []TypSummaryDetails
+}
+
+type TypSummaryDetails struct {
 	location string
 	count    int
 }
