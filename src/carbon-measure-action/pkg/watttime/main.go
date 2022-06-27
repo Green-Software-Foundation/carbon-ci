7295f71e9e7bfeba08e1ec6fa83429df09c0bc3d
Szymon Duchniewicz <szymon.duchniewicz@avanade.com>
2022-06-27 17:30:22 +0100

    fix: fixed merge errors
    Signed-off-by: Szymon Duchniewicz <szymon.duchniewicz@avanade.com>

--- src/carbon-measure-action/pkg/watttime/main.go
+++ src/carbon-measure-action/pkg/watttime/main.go
@@ -4,7 +4,6 @@ package watttime
 
 import (
 	"encoding/base64"
-	"fmt"
 	"main/pkg/http"
 	"strconv"
 )
@@ -16,7 +15,6 @@ var token string
 // Login obtain an access token, it returns an error if failed.
 func Login(username string, password string) error {
 	header := make(map[string]string)
-
 	header["Authorization"] = "Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))
 
 	var response loginResp
@@ -29,7 +27,6 @@ func Login(username string, password string) error {
 	}
 
 	err := request.Send()
-	fmt.Println(err)
 	if err != nil {
 		return err
 	}
